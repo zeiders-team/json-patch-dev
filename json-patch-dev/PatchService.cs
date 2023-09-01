c71e0670686b1306eafeb76f25fa752e39674211
zeiders <zeiders@illinois.edu>
2023-09-01 14:38:23 -0500

    Add project files.

---
+++ json-patch-dev/PatchService.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.NetworkInformation;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using Json.Patch;
+
+namespace json_patch_dev
+{
+    public class PatchService
+    {
+
+        public string Compare (string jsonSource, string jsonDest)
+        {
+            var source = Parse(jsonSource).RootElement;
+            var dest = Parse(jsonDest).RootElement;
+
+			var patch = source.CreatePatch(dest);
+			return Serialize(patch);
+
+        }
+
+        public string Patch (string jsonSource, string jsonPatch)
+        {
+			JsonPatch patch;
+			try
+			{
+				patch = JsonSerializer.Deserialize<JsonPatch>(jsonPatch) ?? throw new ArgumentException(nameof(jsonPatch), "Unable to deserialize JSON Patch");
+			}
+			catch (Exception e)
+			{
+				throw;
+			}
+
+		    var source = Parse(jsonSource);
+			var result = patch.Apply(source) ?? throw new NullReferenceException("Error applying patch");
+
+			return Serialize(result);
+        }
+
+		private JsonDocument Parse(string json) 
+		{
+			JsonDocument jsonDoc;
+
+		    try
+		    {
+			    jsonDoc = JsonDocument.Parse(json);
+		    }
+		    catch (Exception e)
+		    {
+                throw;
+		    }
+
+			return jsonDoc;
+		}
+
+		private string Serialize(object obj)
+		{
+			return  JsonSerializer.Serialize(obj, new JsonSerializerOptions
+			{
+				WriteIndented = true,
+				Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+			});
+		}
+    }
+}
---
+++ json-patch-devTests1/PatchServiceTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using json_patch_dev;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace json_patch_dev.Tests
+{
+    [TestClass()]
+    public class PatchServiceTests
+    {
+
+        [TestMethod()]
+        public void CompareTest()
+        {
+            PatchService patchService = new PatchService();
+
+            var objA = new {
+                scalar=1,
+                array=new [] {"hello" },
+                obj= new { a=1, b=2, c=3, d=new { x=1,y=2 } }
+                };
+
+            var objB = new {
+                scalar=2,
+                array=new [] {"hello","goodbye" },
+                obj= new { a=1, b=0, d=new { x=1,y=3 }, e=5 }
+                };
+
+            var jsonA = JsonSerializer.Serialize(objA);
+            var jsonB = JsonSerializer.Serialize(objB);
+
+            var patch = patchService.Compare(jsonA, jsonB);
+
+            Console.WriteLine(patch);
+            Assert.IsTrue(!string.IsNullOrWhiteSpace(patch));
+        }
+
+        [TestMethod()]
+        public void PatchTest()
+        {
+            PatchService patchService = new PatchService();
+
+            var objA = new {
+                scalar=1,
+                array=new [] {"hello" },
+                obj= new { a=1, b=2, c=3 }
+                };
+
+            var patch = @"[
+                {
+                ""op"": ""replace"",
+                ""path"": ""/scalar"",
+                ""value"": 2
+                },
+                {
+                ""op"": ""add"",
+                ""path"": ""/array/1"",
+                ""value"": ""goodbye""
+                },
+                {
+                ""op"": ""remove"",
+                ""path"": ""/obj/c""
+                },
+                {
+                ""op"": ""add"",
+                ""path"": ""/obj/d"",
+                ""value"": 4
+                },
+                {
+                ""op"": ""replace"",
+                ""path"": ""/obj/b"",
+                ""value"": 0
+                }
+            ]";
+
+            var jsonA = JsonSerializer.Serialize(objA);
+            //var jsonPatch =  JsonSerializer.Serialize(patch);
+            //Console.WriteLine(jsonPatch);
+
+            var jsonB = patchService.Patch(jsonA, patch);
+
+            Console.WriteLine(jsonB);
+            Assert.IsTrue(!string.IsNullOrWhiteSpace(jsonB));
+        }
+    }
+}
